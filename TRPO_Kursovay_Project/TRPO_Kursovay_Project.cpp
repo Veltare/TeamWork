0c13d9a4ff2535c779a0b3ec1ff85f40522fc6ce
Veltare <ya.pcaristokrat@yandex.ru>
2017-04-16 13:26:13 +0700

    basic commit(zakladka fundamenta)

---
+++ TRPO_Kursovay_Project/TRPO_Kursovay_Project.cpp
@@ -0,0 +1,16 @@
+// TRPO_Kursovay_Project.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <clocale>
+#include <ctime>
+#include <fstream>
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	system("pause");
+    return 0;
+}
+
