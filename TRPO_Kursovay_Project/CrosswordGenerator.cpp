f84f9e872ebf1bbcce6c7d3eb914fa789ce0ef65
Veltare <ya.pcaristokrat@yandex.ru>
2017-05-01 21:32:27 +0700

    polnay peredelka algoritma nachal s nula

---
+++ TRPO_Kursovay_Project/CrosswordGenerator.cpp
@@ -0,0 +1,19 @@
+// TRPO_Kursovay_Project.cpp: определяет точку входа для консольного приложения.
+//
+#include "stdafx.h"
+#include <iostream>
+#include <clocale>
+#include <fstream>
+#include <string>
+#include <iomanip>
+#include <algorithm>
+
+#include "CrosswordGenerator.h"
+
+using namespace std;
+
+//string sharedLetters(string s1,string s2)
+
+//{
+//	return sharedletters;
+//}
---
+++ TRPO_Kursovay_Project/CrosswordGeneratorLAUNCH.cpp
@@ -0,0 +1,56 @@
+#include "stdafx.h"
+#include <iostream>
+#include <clocale>
+#include <fstream>
+#include <string>
+#include <vector>
+#include "CrosswordGenerator.h"
+
+using namespace std;
+
+int main()
+{
+	const int size = 52;
+
+
+	ifstream wordList;
+	wordList.open("WordBase.txt");
+
+	string words[size];
+
+	vector<vector<string>> crossword;
+
+	crossword.resize(size);
+
+	for (int z = 0; z < size; z++)
+	{
+		crossword[z].resize(size);
+	}
+
+
+	string line;
+	int count = 0;
+	while (getline(wordList, line))
+	{
+		words[count] = line;
+		count++;
+	}
+
+	wordList.close();
+
+	for (int i = 0; i < size; i++)
+	{
+		for (int comparisonIndex = i + 1; comparisonIndex < size; comparisonIndex++)
+		{
+
+			if (words[i].length() < words[comparisonIndex].length())
+			{
+				string biggerWord = words[comparisonIndex];
+
+				words[comparisonIndex] = words[i];
+				words[i] = biggerWord;
+			}
+		}
+
+	}
+}
