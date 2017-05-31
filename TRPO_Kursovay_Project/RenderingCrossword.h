bf1015ae60692636bcb8250f903d8e8810fbe34b
Veltare <ya.pcaristokrat@yandex.ru>
2017-05-31 17:23:46 +0700

    BugFixBugFix

--- TRPO_Kursovay_Project/RenderingCrossword.h
+++ TRPO_Kursovay_Project/RenderingCrossword.h
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 #pragma once
 
 #ifndef RenderingCrossword_H
@@ -16,25 +16,3 @@ void DisplayBoards(char board[][200], int x_min, int x_max, int y_min, int y_max
 int VictoryCondition(struct base *basic_base, int size);
 #endif // !RENDERING_CROSSWORD_H
 
-
-||||||| merged common ancestors
-=======
-#pragma once
-
-#ifndef RenderingCrossword_H
-#define RenderingCrossword_H
-
-using namespace std;
-
-void initilizeBoard(char board[][100], char c);
-void initilizeBoard(char board[][200], char c);
-void CreateBoards(char board[][200], char solution[][100], int **location, int wordcount, string words[], struct base *basic_base);
-void displayQuestions(string arg[], int size);
-int MainManu(int size, struct base *basic_base, char board[][200]);
-int InputInCrossword(char board[][200], int x, int y, string word, int orintation);
-void DisplayBoards(char board[][200]);
-int VictoryCondition(struct base *basic_base, int size);
-#endif // !RENDERING_CROSSWORD_H
-
-
->>>>>>> 295ead2851cf59d27d7af4d7d4f26a09647e30fe
